08dffe1fc45ab24e8bc5123a89caef03f33858b4
meatpuppet <jan@kwoh.de>
2014-09-23 15:29:08 +0200

    chunked uploads!

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ from datetime import datetime
 from flask import Flask, render_template, jsonify, redirect, url_for, request, send_from_directory
 from flask_bootstrap import Bootstrap
 import time
+import tempfile
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['UPLOAD_FOLDER'] = os.path.normpath('uploads')
+app.config['UPLOAD_FOLDER'] = os.path.normpath('/home/meatpuppet/Daten/Up/')
+tmp_prefix='.tmp_'
+
 Bootstrap(app)
 
-ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'svg', 'zip', 'mp3', 'rar', 'tar', 'gz']
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'svg', 'zip', 'mp3', 'rar', 'tar', 'gz', 'deb', 'avi',
+        'mpg', 'mkv']
+
 
 class filething():
     def __init__(self, path):
@@ -40,9 +45,58 @@ def index():
     return render_template("index.html", files=filelist, extensions=ALLOWED_EXTENSIONS)
 
 
+def mvToUploadDir(filename):
+    now = datetime.now()
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], "%s" % (now.strftime("%Y-%m-%d-%H-%M-%S-%f")))
+    os.mkdir(filepath)
+    os.rename(os.path.join(app.config['UPLOAD_FOLDER'], tmp_prefix + filename), os.path.join(filepath, filename) )
+    pass
+
+
 @app.route('/upload', methods=['POST'])
 def upload():
+
     if request.method == 'POST':
+        files = request.files
+
+        # assuming only one file is passed in the request
+        key = files.keys()[0]
+        value = files[key] # this is a Werkzeug FileStorage object
+        filename = value.filename
+
+        if 'Content-Range' in request.headers:
+            # extract starting byte from Content-Range header string
+            range_str = request.headers['Content-Range']
+            start_bytes = int(range_str.split(' ')[1].split('-')[0])
+            end_bytes= int(range_str.split(' ')[1].split('-')[1].split('/')[0])
+            fsize = int(range_str.split(' ')[1].split('-')[1].split('/')[1])
+            # append chunk to the file on disk, or create new
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], tmp_prefix + filename), 'a') as f:
+                f.seek(start_bytes)
+                f.write(value.stream.read())
+
+            if (fsize - end_bytes) == 1:
+                mvToUploadDir(filename)
+
+
+        else:
+            # this is not a chunked request, so just save the whole file
+            value.save(filename)
+            mvToUploadDir(filename)
+
+            # send response with appropriate mime type header
+        return jsonify({"name": value.filename,
+                    "size": os.path.getsize(filename),
+                    "url": 'uploads/' + value.filename,
+                    "thumbnail_url": None,
+                    "delete_url": None,
+                    "delete_type": None,})
+
+
+
+
+'''
+        print tempfile.gettempdir()
         file = request.files['file']
         if file and allowed_file(file.filename):
             now = datetime.now()
@@ -57,7 +111,7 @@ def upload():
             #return redirect(url_for('index'))
         else:
             return jsonify(error='ext name error')
-
+'''
 
 @app.route('/downloads/<path:filename>', methods=['GET', 'POST'])
 def download(filename):
